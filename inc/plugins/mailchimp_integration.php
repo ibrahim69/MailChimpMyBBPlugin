efa5ee3d6a5b0a06d38974e2cbccf10100caaf1e
desgyz <audibailey7@gmail.com>
2015-06-27 12:15:41 +1000

    Main Plugin File.

---
+++ inc/plugins/mailchimp_integration.php
@@ -0,0 +1,221 @@
+<?php 
+function mailchimp() {
+	    
+	global $mybb, $db, $user_info, $userfields;
+	if ($mybb->settings['mailchimp_onoff'] == "1") {
+		$MailChimp = '';
+		$api_key = '';
+		$list_id = '';
+		$double_optin = '';
+		
+		$user_id = '';
+		$user_name = '';
+		$user_email = '';
+		$user_ip = '';
+		$optinTIME = '';
+		$merge_vars = '';
+		$retval = '';
+		$parameters = '';
+		
+		$user = get_user($uid);
+		
+		$api_key = $mybb->settings['mailchimp_api'];
+		$list_id =  $mybb->settings['mailchimp_listid'];
+		$fieldnum = $mybb->settings['mailchimp_field'];
+		$api_loc = $mybb->settings['mailchimp_apiadd'];
+		$confmsg = $mybb->settings['mailchimp_conf'];
+		
+		$getuserinfo = $db->simple_select(
+			"users",
+			"uid, username, email, regip ",
+			"uid=" . (int)$user_info['uid'] . ""
+		);
+		$userInfo = $db->fetch_array($getuserinfo);
+		$user_id = $userInfo['uid'];
+		$user_name = $userInfo['username'];
+		$user_email = $userInfo['email'];
+		$user_ip = $userInfo['regip'];
+		$optinTIME = date('Y-m-d H:i:s');
+
+		if ($mybb->settings['mailchimp_opting']){
+			$useroptq = $db->query("SELECT fid" . $fieldnum . " FROM mybb_userfields WHERE ufid=" . $user_id . "");
+			$user_opt = $db->fetch_array($useroptq, 'fid'. $fieldnum);
+			$user_opt = $user_opt['fid'. $fieldnum];
+		} else {
+			$user_opt = $confmsg;
+		}
+		
+		$newmem = array(
+					"email_address" => $user_email, 
+					"status" => "subscribed", 
+					"merge_fields" => array(
+						"UNAME" => $user_name, 
+					)
+				  );
+		$jsonnewmem = json_encode($newmem);
+		$apifresh = 'https://'. $api_loc .'.api.mailchimp.com/3.0/lists/'. $list_id . '/members/';
+		if ($user_opt == $confmsg){
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,$apifresh);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+														'Authorization: Basic '.$api_key));
+			curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonnewmem);                                                                                                                  
+			$result = curl_exec($ch);	
+			/* print('Opt' . $user_opt);
+			print('success');
+			var_dump('Result'. $result);
+			print('loc' . $api_loc);
+			print('conf msg'. $confmsg); */
+		} else {
+			/* var_dump($user_opt); */
+		}
+	}
+}
+
+$plugins->add_hook("member_do_register_end", "mailchimp");
+
+function mailchimp_integration_info(){
+    return array(
+        "name"          => "Mailchimp",
+        "description"   => "Mailchimp",
+        "website"       => "htttps://mailchimp.com",
+        "author"        => "Desgyz",
+        "authorsite"    => "https://google.com",
+        "version"       => "1.0.2",
+        "guid"      => "",
+        "compatibility" => "*"
+    );
+}
+
+function mailchimp_integration_activate(){
+    global $db,$mybb;
+    $settingsGroup = array(
+        "gid" => "NULL",
+        "name" => "mailchimp",
+        "title" => "Mailchimp Integration",	
+        "description" => "Enable or disable of MailChimp email delivery service integration.",
+        "disporder" => "50",
+        "isdefault" => "no",
+    );
+    $db->insert_query("settinggroups", $settingsGroup);
+    $gid = $db->insert_id();
+
+    $settingOnOff = array(
+        "sid" => "NULL",
+        "name" => "mailchimp_onoff",
+        "title" => "Plugin Activation",
+        "description" => "Set to YES to enable Mailchimp email service integration.",
+        "optionscode" => "yesno",
+        "value" => "0",
+        "disporder" => "1",
+        "gid" => intval($gid),
+    );
+    $db->insert_query("settings", $settingOnOff);
+
+	
+	$settingOpting = array(
+		"sid" => "NULL",
+		"name" => "mailchimp_opting",
+		"title" => "Opt-In Setting",
+		"description" => "Enable/Disable Opt-In settings on the registration",
+		"optionscode" => "yesno",
+		"value" => "1",
+		"disporder" => "4",
+		"gid" => intval($gid),
+	);
+	$db->insert_query("settings", $settingOpting);
+
+	$settingAPIadd = array(
+		"sid" => "NULL",
+		"name" => "mailchimp_apiadd",
+		"title" => "Location Server API Key",
+		"description" => "The last 3 to 4 character on end of api key (after the hyphen). Eg: us10, us2",
+		"optionscode" => "text",
+		"value" => "us9",
+		"disporder" => "7",
+		"gid" => intval($gid),
+	);
+	$db->insert_query("settings", $settingAPIadd);
+	
+	$settingCustomField = array(
+		"sid" => "NULL",
+		"name" => "mailchimp_field",
+		"title" => "Custom Field ID",
+		"description" => "Custom Field id for the checkbox or radio button opt-in setting.",
+		"optionscode" => "text", 
+		"value" => "4",
+		"disporder" => "5",
+		"gid" => intval($gid),
+	);
+	$db->insert_query("settings", $settingCustomField);	
+	
+	$settingConfirmMessage = array(
+		"sid" => "NULL",
+		"name" => "mailchimp_conf",
+		"title" => "Custom Field Text",
+		"description" => "The text for the checkbox/radio button option.",
+		"optionscode" => "text", 
+		"value" => "Yes?",
+		"disporder" => "8",
+		"gid" => intval($gid),
+	);
+	$db->insert_query("settings", $settingConfirmMessage);
+	
+	/*
+	$settingDoubleOptin = array(
+        "sid" => "NULL",
+        "name" => "mailchimp_doubleoptin",
+        "title" => "Double opt-in confirmation",
+        "description" => "Flag to control whether a double opt-in confirmation message is sent, defaults to true. Abusing this may cause your Mailchimp account to be suspended.",
+        "optionscode" => "yesno",
+        "value" => "1",
+        "disporder" => "4",
+        "gid" => intval($gid),
+    );
+    $db->insert_query("settings", $settingDoubleOptin);*/
+	
+    $settingApi = array(
+        "sid" => "NULL",
+        "name" => "mailchimp_api",
+        "title" => "Mailchimp API Key",
+        "description" => "Please enter the API Key retrieved from here: http://admin.mailchimp.com/account/api/ ",
+        "optionscode" => "text",
+        "value" => "123456789-us2",
+        "disporder" => "2",
+        "gid" => intval($gid),
+    );
+    $db->insert_query("settings", $settingApi);
+    
+	$settingListID = array(
+        "sid" => "NULL",
+        "name" => "mailchimp_listid",
+        "title" => "Mailchimp List ID",
+        "description" => "Please enter the List Unique Id obtained by going to: http://admin.mailchimp.com/lists/ <br /><small>Click the \"setting\" -> \"List name & defaults\" for the list - the Unique Id is at this page.</small>",
+        "optionscode" => "text",
+        "value" => "abcdefgh",
+        "disporder" => "3",
+        "gid" => intval($gid),
+    );
+    $db->insert_query("settings", $settingListID);
+	
+    rebuild_settings();
+}
+
+function mailchimp_integration_deactivate()
+{
+    global $db;
+    $db->delete_query("settinggroups", "name='mailchimp'");
+    $db->delete_query(
+        "settings",
+        "name IN(
+            'mailchimp_onoff', 'mailchimp_api', 'mailchimp_listid', 'mailchimp_opting', 'mailchimp_field', 'mailchimp_apiadd', 'mailchimp_conf'
+        )"
+    );
+    rebuild_settings();
+}
+?>
