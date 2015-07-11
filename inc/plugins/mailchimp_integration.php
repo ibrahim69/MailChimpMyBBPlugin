df62a9fe66964d8931aec87a9fee213126f3565c
desgyz <audibailey7@gmail.com>
2015-07-11 13:11:47 +1000

    Fixed the donation button
    Donation plugin no longer links to blank paypal page.

--- inc/plugins/mailchimp_integration.php
+++ inc/plugins/mailchimp_integration.php
@@ -85,11 +85,17 @@ function mailchimp_integration_info(){
         "name"          => "Mailchimp",
         "description"   =>  '
 	<p> Mailchimp Plugin for MyBB</p>
+
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-		<input type="hidden" name="cmd" value="_s-xclick">
-		<input type="hidden" name="hosted_button_id" value="">
-		<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-		<img alt="" border="0" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif\" width="1" height="1">
+		<input type="hidden" name="cmd" value="_donations">
+		<input type="hidden" name="business" value="michaelengelandreasen@gmail.com">
+		<input type="hidden" name="lc" value="AU">
+		<input type="hidden" name="item_name" value="Mailchimp Mybb Plugin">
+		<input type="hidden" name="no_note" value="0">
+		<input type="hidden" name="currency_code" value="USD">
+		<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
+		<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+		<img alt="" border="0" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif" width="1" height="1">
 	</form>
 ',
         "website"       => "htttps://mailchimp.com",
